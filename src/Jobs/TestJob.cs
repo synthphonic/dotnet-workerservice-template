0061cfd0bb896c2f51797db61ec7b1458b9ccdf0
Shah Z. S <shahzshafie@outlook.com>
2021-08-20 17:35:22 +0800

    first checkin for .net work service template

---
+++ src/Jobs/TestJob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Coravel.Invocable;
+using Microsoft.Extensions.Logging;
+
+namespace WorkerServiceTemplate.Jobs
+{
+    public class TestJob : IInvocable
+    {
+        private readonly ILogger<TestJob> _logger;
+
+        public TestJob(ILogger<TestJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Invoke()
+        {
+            var jobId = Guid.NewGuid();
+            _logger.LogInformation($"Starting job Id {jobId}");
+
+            await Task.Delay(3000);
+
+            _logger.LogInformation($"Job with Id {jobId} completed");
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Coravel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkerServiceTemplate.Jobs;
+
+namespace WorkerServiceTemplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Services.UseScheduler(scheduler =>
+            {
+                var jobSchedule = scheduler.Schedule<TestJob>();
+                jobSchedule
+                    .EverySeconds(2)
+                    .PreventOverlapping("TestJob");
+            });
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // comment out if using coravel
+                    //services.AddHostedService<Worker>();
+
+                    services.AddScheduler();
+                    services.AddTransient<TestJob>();
+                });
+    }
+}
